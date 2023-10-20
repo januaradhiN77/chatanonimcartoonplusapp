66259371e68cc1cd69d1307ef02abb136c317b4d
adhi <adhinugroho623@gmail.com>
2023-10-21 06:40:38 +0700

    update lagi

--- src/components/Mapel/Sabtu.jsx
+++ src/components/Mapel/Sabtu.jsx
@@ -15,7 +15,7 @@ const Sabtu = () => {
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60 border-b-2 border-white" data-aos="fade-up" data-aos-duration="900">
 					<div className="w-[50%] text-base  font-medium">Informatika</div>
-					<div className="flex justify-center items-center text-sm">10.43-11.4340</div>
+					<div className="flex justify-center items-center text-sm">10.43-11.43</div>
 				</div>
 			
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
