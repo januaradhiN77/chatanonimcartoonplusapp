f4edd2a887aa2f98473f1a9bab2ce0be7a16662b
adhidanmama <adhidanmama@gmail.com>
2024-07-14 15:58:45 +0000

    99

--- src/components/ChatAnonim.jsx
+++ src/components/ChatAnonim.jsx
@@ -201,7 +201,7 @@ const Chat = () => {
     />
   </div>
   <button onClick={sendMessage} id="send" className="ml-2 bg-transparent w-full placeholder:text-white placeholder-opacity-60">
-    <img src="/paper-plane.png" alt="Send" className="h-6 w-6 lg:h-6 lg:w-6 color-white" />
+    <img src="/paper-plane.png" alt="Send" className="h-5 w-5 lg:h-6 lg:w-6 color-white" />
   </button>
 </div>
 
